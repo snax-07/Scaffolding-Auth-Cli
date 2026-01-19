9f98a8c928d3a953212032110c2a3bc16864a8e9
snax <swapnilnade07@gmail.com>
2026-01-19 12:17:39 +0530

    Specific module build

--- src/template/module/js/passwordReset.js
+++ src/template/module/js/passwordReset.js
@@ -1,8 +1,36 @@
-export const passwordReset = async () => {
+
+export const passwordReset = async (req , res) => {
     try {
-        
+        const {newPassword , oldPassword} = req.body;
+        if(!newPassword || !oldPassword){
+            return res.status(209).json({
+                message : "Provide all parameteres !!!",
+                success : false
+            });
+        }
+
+        const user = await User.findOne({
+            email : req.user.email
+        });
+        if(bcrypt.compare(newPassword , user.password)){
+            return res.status(200).json({
+                message : "Password is already in use !!!",
+                success : false
+            });
+        }
+
+        user.password = bcrypt.hashSync(newPassword , 10);
+        await user.save();
+
+
+        return res.status(200).json({
+            message : "Password changed successfully !!!",
+            status : true
+        })
     } catch (error) {
-        console.log("Error while reeting the password !!!"),
-        process.exit(0);
+        return res.status(500).json({
+            message : "Password reset error !!!",
+            success : false
+        })
     }
 }
---
+++ src/template/module/ts/passwordReset.ts
@@ -0,0 +1,37 @@
+
+
+export const passwordReset = async (req : Request< {} , {} , {} , {newPassword : string , oldPassword : string}> , res : Response) => {
+    try {
+        const {newPassword , oldPassword} = req.body;
+        if(!newPassword || !oldPassword){
+            return res.status(209).json({
+                message : "Provide all parameteres !!!",
+                success : false
+            });
+        }
+
+        const user = await User.findOne({
+            email : req.user.email
+        });
+        if(bcrypt.compare(newPassword , user.password)){
+            return res.status(200).json({
+                message : "Password is already in use !!!",
+                success : false
+            });
+        }
+
+        user.password = bcrypt.hashSync(newPassword , 10);
+        await user.save();
+
+
+        return res.status(200).json({
+            message : "Password changed successfully !!!",
+            status : true
+        })
+    } catch (error : any) {
+        return res.status(500).json({
+            message : "Password reset error !!!",
+            success : false
+        })
+    }
+}
